4011e9ec226ad9bced824a90848f971d27af7bf5
aseemgupta <aseemgupta2625@gmail.com>
2019-06-07 01:22:23 +0530

    Add classList and prefixToClasses utils

--- src/_String.js
+++ src/_String.js
@@ -36,6 +36,51 @@ export function snakeCaseToTitleCase(str = '', splitToken = '_') {
   return stringToTitleCase(string, '_');
 }
 
+/**
+ * A function to create space separated classes. If value is truthy the value returned by expression is appended as a class.
+ * @param  {expressions} ...args Arguments where each are falsy/truthy.
+ * @return {String}                           A space separated string to be used as class
+ * @example classList(falsy_condition1 && 'some-class1', truthy_condition2 && 'some-class2') = 'some-class2'
+ */
+export function classList(...args) {
+  const classes = [];
+
+  for (var i = 0; i < args.length; i++) {
+    if (args[i]) {
+      if (args[i] instanceof Array) {
+        args[i] = args[i].join(' ');
+      }
+
+      classes.push(args[i]);
+    }
+  }
+
+  return classes.join(' ');
+}
+
+/**
+ * A function to add prefix to each in the set of space separated string.
+ * @param  {String} prefix prefix to be prepended
+ * @param  {String} classes Space separated strings to each of which prefix will be prepended
+ * @return {String}         A space separated string where each of classes have prefix prepended to them
+ * @example prefixToClasses('prefix', 'class1 class2') = 'prefix--class1 prefix-class2'
+ */
+export function prefixToClasses(pc, classes = '') {
+  if (!classes) {
+    return '';
+  }
+
+  return classes
+    .split(' ')
+    .reduce((a, c) => {
+      c.trim() && a.push(pc + c);
+
+      return a;
+    }, [])
+    .join(' ');
+}
+
+
 
 /*
 *
